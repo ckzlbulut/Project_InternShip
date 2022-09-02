782d29e5e6b4def87569aea4c614a8b4a687b004
Burak <mikailburak@icloud.com>
2022-09-02 23:43:27 +0300

    Burak yazdı

--- src/test/java/Pages/deneme_burak.java
+++ src/test/java/Pages/deneme_burak.java
@@ -2,6 +2,6 @@ package Pages;
 
 public class deneme_burak {
     public static void main(String[] args) {
-        System.out.println("merhaba grup8");
+        System.out.println("Burak grup8");
     }
 }
